a213340e1520fd08feb5d3e452d798170e6dd578
SnowzTail <i@snowztail.com>
2020-06-16 21:07:31 +0100

    :sparkles: R-E region vs number of subbands

---
+++ src/config_subband.m
@@ -0,0 +1,50 @@
+%% * Transceiver
+% diode k-parameter
+k2 = 0.0034;
+k4 = 0.3829;
+% antenna resistance
+resistance = 50;
+% coefficients on current terms
+beta2 = k2 * resistance;
+beta4 = k4 * resistance ^ 2;
+% number of transmit and receive antennas
+nTxs = 1;
+nRxs = 1;
+% number of users
+nUsers = 1;
+% average transmit power
+txPower = 1;
+% average noise power
+noisePower = db2pow(-50);
+
+%% * Channel
+% AP-user distance
+directDistance = 10;
+% AP-IRS distance
+incidentDistance = 1;
+% IRS-user distance
+reflectiveDistance = directDistance - incidentDistance;
+% center frequency
+centerFrequency = 5.18e9;
+% bandwidth
+bandwidth = 1e6;
+% channel fading mode ("flat" or "selective")
+fadingMode = "selective";
+% gain on each reflecting element
+irsGain = 1;
+% number of reflecting elements in IRS
+nReflectors = 10;
+
+%% * Algorithm
+% minimum gain ratio per iteration
+tolerance = 1e-6;
+%  number of CSCG random vectors to generate
+nCandidates = 1e4;
+% gap between rate samples
+resolution = 5e-2;
+% number of samples in R-E curves
+nSamples = 20;
+
+%% * Variable
+% number of frequency bands
+Variable.nSubbands = 2 .^ (1 : 4);
---
+++ src/re_subband.m
@@ -0,0 +1,73 @@
+clear; clc; setup; config_subband;
+
+% * Tap response
+[directTapGain, directTapDelay] = taps_tgn(nTxs, nRxs);
+[incidentTapGain, incidentTapDelay] = taps_tgn(nTxs, nReflectors);
+[reflectiveTapGain, reflectiveTapDelay] = taps_tgn(nReflectors, nRxs);
+
+% * R-E region vs number of subbands
+directReSample = cell(length(Variable.nSubbands), 1);
+ffReSample = cell(length(Variable.nSubbands), 1);
+for iSubband = 1 : length(Variable.nSubbands)
+    nSubbands = Variable.nSubbands(iSubband);
+    [subbandFrequency] = subband_frequency(centerFrequency, bandwidth, nSubbands);
+
+    % * Direct link
+    [directFading] = fading_tgn(directTapGain, directTapDelay, nSubbands, subbandFrequency, fadingMode);
+    [directPathloss] = path_loss(directDistance, "direct");
+    directChannel = directFading / sqrt(directPathloss);
+
+    % * Incident link
+    [incidentFading] = fading_tgn(incidentTapGain, incidentTapDelay, nSubbands, subbandFrequency, fadingMode);
+    [incidentPathloss] = path_loss(incidentDistance, "incident");
+    incidentChannel = incidentFading / sqrt(incidentPathloss);
+
+    % * Reflective link
+    [reflectiveFading] = fading_tgn(reflectiveTapGain, reflectiveTapDelay, nSubbands, subbandFrequency, fadingMode);
+    [reflectivePathloss] = path_loss(reflectiveDistance, "reflective");
+    reflectiveChannel = reflectiveFading / sqrt(reflectivePathloss);
+
+    %% ! No-IRS: R-E region vs number of subbands
+    % * Initialize algorithm by WIT
+    [directCapacity, subbandPower] = channel_capacity(directChannel, txPower, noisePower);
+    [infoWaveform, powerWaveform, infoRatio, powerRatio] = initialize_waveform_wit(directChannel, subbandPower);
+    rateConstraint = directCapacity : -directCapacity / (nSamples - 1) : 0;
+
+    % * Achievable R-E region without IRS
+    directReSample{iSubband} = zeros(2, nSamples);
+    for iSample = 1 : nSamples
+        [infoWaveform, powerWaveform, infoRatio, powerRatio, current, rate] = waveform_sdr(infoWaveform, powerWaveform, infoRatio, powerRatio, beta2, beta4, txPower, noisePower, rateConstraint(iSample), tolerance, directChannel, nCandidates);
+        directReSample{iSubband}(:, iSample) = [current; rate];
+    end
+
+    %% ! IRS: R-E region vs number of subbands
+    % * Initialize algorithm by WIT
+    isConverged = false;
+    maxRate_ = 0;
+    irs = irsGain * ones(nReflectors, 1);
+    while ~isConverged
+        [compositeChannel, concatVector, concatMatrix] = composite_channel(directChannel, incidentChannel, reflectiveChannel, irs);
+        [compositeCapacity, subbandPower] = channel_capacity(compositeChannel, txPower, noisePower);
+        [infoWaveform, powerWaveform, infoRatio, powerRatio] = initialize_waveform_wit(compositeChannel, subbandPower);
+        [irs, maxRate] = irs_flat_wit(noisePower, concatMatrix, infoWaveform, nCandidates);
+        isConverged = abs(maxRate - maxRate_) / maxRate <= tolerance;
+        maxRate_ = maxRate;
+    end
+    rateConstraint = maxRate : -maxRate / (nSamples - 1) : 0;
+
+    % * Achievable R-E region by FF-IRS
+    ffReSample{iSubband} = zeros(2, nSamples);
+    for iSample = 1 : nSamples
+        isConverged = false;
+        current_ = 0;
+        [infoWaveform, powerWaveform, infoRatio, powerRatio, current, rate] = waveform_sdr(infoWaveform, powerWaveform, infoRatio, powerRatio, beta2, beta4, txPower, noisePower, rateConstraint(iSample), tolerance, compositeChannel, nCandidates);
+        while ~isConverged
+            [irs, currentInit, rateInit] = irs_flat(irs, beta2, beta4, noisePower, rateConstraint(iSample), tolerance, concatVector, concatMatrix, infoWaveform, powerWaveform, infoRatio, powerRatio, nCandidates);
+            [compositeChannel, concatVector, concatMatrix] = composite_channel(directChannel, incidentChannel, reflectiveChannel, irs);
+            [infoWaveform, powerWaveform, infoRatio, powerRatio, current, rate] = waveform_sdr(infoWaveform, powerWaveform, infoRatio, powerRatio, beta2, beta4, txPower, noisePower, rateConstraint(iSample), tolerance, compositeChannel, nCandidates);
+            isConverged = abs(current - current_) / current <= tolerance;
+            current_ = current;
+        end
+        ffReSample{iSubband}(:, iSample) = [current; rate];
+    end
+end
