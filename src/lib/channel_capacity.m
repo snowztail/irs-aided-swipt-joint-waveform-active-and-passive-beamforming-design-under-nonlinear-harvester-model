01b4aaee73559880a82ebe2e7ee3f6491385af24
SnowzTail <i@snowztail.com>
2020-08-01 00:45:45 +0100

    :ambulance: Fix index mismatch

--- src/lib/channel_capacity.m
+++ src/lib/channel_capacity.m
@@ -3,7 +3,7 @@ function [capacity, infoWaveform] = channel_capacity(channel, txPower, noisePowe
     %   - calculate the maximum achievable rate based on water-filling power allocation
     %
     % Input:
-    %   - channel (h) [nSubbands * nTxs * nRxs]: the wireless channel
+    %   - channel (h) [nSubbands * nTxs * nRxs]: channel frequency response
     %   - txPower (P): average transmit power
     %   - noisePower (\sigma_n^2): average noise power
     %
@@ -13,22 +13,42 @@ function [capacity, infoWaveform] = channel_capacity(channel, txPower, noisePowe
     %
     % Comment:
     %   - for MISO OFDM channels
-    %   - multiply 2 in power allocation to convert effective value to magnitude
+    %   - multiply 2 in power allocation to convert average power to maximum power
+    %   - this is because wireless communication always transmit at the peak of waveform (maybe a different definition of transmit power budget)
     %
     % Author & Date: Yang (i@snowztail.com) - 28 Aug 19
 
 
+    % * Get data
+    nSubbands = size(channel, 1);
 
-    % * MRT within each subband
-    subbandStrength = vecnorm(channel, 2, 2) .^ 2;
+    % * Convert average power to maximum power (different definition, transmit at peaks)
+    txPower = 2 * txPower;
+    snr = txPower / noisePower;
 
-    % * Water-filling power allocation
-    subbandPower = water_filling(subbandStrength, 2 * txPower, noisePower);
+    % * Obtain subchannel strength (MRT within each subband)
+    strength = vecnorm(channel, 2, 2) .^ 2;
+
+    % * Iterative water-filling power allocation
+    subbandPower = zeros(nSubbands, 1);
+    [strength, index] = sort(strength, 'descend');
+    baseLevel = 1 ./ (snr * strength);
+    for iSubband = 1 : nSubbands
+        waterLevel = 1 / (nSubbands - iSubband + 1) * (1 + sum(baseLevel(1 : (nSubbands - iSubband + 1))));
+        candidate = waterLevel - baseLevel(1 : iSubband);
+        isValid = all(candidate >= 0);
+        if isValid
+            subbandPower(1 : iSubband) = candidate;
+        else
+            break;
+        end
+    end
+    subbandPower(index) = txPower * (subbandPower ./ sum(subbandPower));
 
     % * Construct waveform
     infoWaveform = sqrt(transpose(subbandPower)) .* channel' ./ vecnorm(channel, 2, 2)';
 
     % * Compute capacity
-    capacity = sum(log2(1 + subbandPower .* subbandStrength / noisePower));
+    capacity = sum(log2(1 + subbandPower .* strength / noisePower));
 
 end
