e225daaffcb43fc814f0f1bfb635743efa46bea5
SnowzTail <i@snowztail.com>
2020-08-05 01:02:55 +0100

    :card_file_box: Archive unused code

---
+++ src/lib/waveform_sdr.m
@@ -0,0 +1,126 @@
+function [infoWaveform, powerWaveform] = waveform_sdr(beta2, beta4, channel, infoWaveform, powerWaveform, txPower, nCandidates, tolerance)
+    % Function:
+    %   - optimize the information and power waveform to maximize the R-E region
+    %
+    % Input:
+    %   - beta2: coefficients on second-order current terms
+    %   - beta4: coefficients on fourth-order current terms
+    %   - channel (h) [nSubbands * nTxs * nRxs]: channel frequency response
+    %   - infoWaveform (w_I) [nTxs * nSubbands]: weight on information waveform (in the previous iteration)
+    %   - powerWaveform (w_P) [nTxs * nSubbands]: weight on power waveform (in the previous iteration)
+    %   - txPower (P): transmit power constraint
+    %   - nCandidates (Q): number of CSCG random vectors to generate
+    %   - tolerance (\epsilon): minimum gain ratio per iteration
+    %
+    % Output:
+    %   - infoWaveform (w_I) [nTxs * nSubbands]: weight on information waveform
+    %   - powerWaveform (w_P) [nTxs * nSubbands]: weight on power waveform
+    %
+    % Comment:
+    %   - suitable for WPT
+    %   - solve SDR problem to obtain high-rank waveform outer product matrices
+    %   - use Gaussian randomization method to extract waveform vectors
+    %
+    % Author & Date: Yang (i@snowztail.com) - 31 May 20
+
+
+    % * Get data
+    [nSubbands] = size(channel, 1);
+
+    % * Initialize algorithm
+    % \boldsymbol{a}, \boldsymbol{s}_{I/P}
+    channelAmplitude = vecnorm(channel, 2, 2);
+
+    % * Construct coefficient matrices
+    % \boldsymbol{S}_{I/P}
+    infoMatrix = vec(infoAmplitude) * vec(infoAmplitude)';
+    powerMatrix = vec(powerAmplitude) * vec(powerAmplitude)';
+
+    % \boldsymbol{A}_n
+    channelMatrix = channelAmplitude * channelAmplitude';
+    channelBlkDiag = block_diagonal(channelMatrix, 1, nSubbands);
+
+    % * Initialize auxiliaries
+    % t'_{I/P,n}
+    infoAuxiliary = zeros(2 * nSubbands - 1, 1);
+    powerAuxiliary = zeros(2 * nSubbands - 1, 1);
+    for iSubband = - nSubbands + 1 : nSubbands - 1
+        infoAuxiliary(iSubband + nSubbands) = trace(channelBlkDiag{iSubband + nSubbands} * infoMatrix);
+        powerAuxiliary(iSubband + nSubbands) = trace(channelBlkDiag{iSubband + nSubbands} * powerMatrix);
+    end
+
+    % * SCA
+    current_ = 0;
+    isConverged = false;
+    while ~isConverged
+        % * Update auxiliaries
+        infoAuxiliary_ = infoAuxiliary;
+        powerAuxiliary_ = powerAuxiliary;
+
+        % * Solve high-rank outer product matrix by CVX
+        cvx_begin quiet
+            cvx_solver mosek
+            variable infoMatrix(nSubbands, nSubbands) hermitian semidefinite;
+            variable powerMatrix(nSubbands, nSubbands) hermitian semidefinite;
+            expression infoAuxiliary(2 * nSubbands - 1, 1);
+            expression powerAuxiliary(2 * nSubbands - 1, 1);
+            % t'_{I/P,n}
+            for iSubband = - nSubbands + 1 : nSubbands - 1
+                infoAuxiliary(iSubband + nSubbands) = trace(channelBlkDiag{iSubband + nSubbands} * infoMatrix);
+                powerAuxiliary(iSubband + nSubbands) = trace(channelBlkDiag{iSubband + nSubbands} * powerMatrix);
+            end
+            % \tilde{z}
+            currentSca = (1 / 2) * beta2 * (infoAuxiliary(nSubbands) + powerAuxiliary(nSubbands)) ...
+                + (3 / 8) * beta4 ^ 2 * (2 * (2 * infoAuxiliary(nSubbands) * infoAuxiliary_(nSubbands) - infoAuxiliary_(nSubbands) ^ 2) + 2 * real(powerAuxiliary_' * powerAuxiliary) - powerAuxiliary_' * powerAuxiliary_) ...
+                + (3 / 2) * beta4 ^ 2 * ((1 / 2) * (infoAuxiliary(nSubbands) + powerAuxiliary(nSubbands)) * (infoAuxiliary_(nSubbands) + powerAuxiliary_(nSubbands)) - (1 / 4) * (infoAuxiliary_(nSubbands) + powerAuxiliary_(nSubbands)) ^ 2 - (1 / 4) * (infoAuxiliary(nSubbands) - powerAuxiliary(nSubbands)) ^ 2);
+            maximize currentSca;
+            subject to
+                (1 / 2) * (trace(infoMatrix) + trace(powerMatrix)) <= txPower;
+        cvx_end
+
+        % * Update output current
+        current = (1 / 2) * beta2 * (infoAuxiliary(nSubbands) + powerAuxiliary(nSubbands)) ...
+            + (3 / 8) * beta4 ^ 2 * (2 * infoAuxiliary(nSubbands) ^ 2 + (powerAuxiliary' * powerAuxiliary)) ...
+            + (3 / 2) * beta4 ^ 2 * infoAuxiliary(nSubbands) * powerAuxiliary(nSubbands);
+
+        % * Test convergence
+        isConverged = abs(current - current_) <= tolerance;
+        current_ = current;
+    end
+    infoMatrix = full(infoMatrix);
+    powerMatrix = full(powerMatrix);
+
+    % * Recover rank-1 solution by randomization method
+    [u1, sigma1] = eig(infoMatrix);
+    [u2, sigma2] = eig(powerMatrix);
+    current = 0;
+    for iCandidate = 1 : nCandidates
+        infoAmplitudeCandidate = u1 * sigma1 ^ (1 / 2) * exp(1i * 2 * pi * rand(nSubbands, 1));
+        powerAmplitudeCandidate = u2 * sigma2 ^ (1 / 2) * exp(1i * 2 * pi * rand(nSubbands, 1));
+        infoMatrix = infoAmplitudeCandidate * infoAmplitudeCandidate';
+        powerMatrix = powerAmplitudeCandidate * powerAmplitudeCandidate';
+
+        % * Update auxiliaries
+        for iSubband = - nSubbands + 1 : nSubbands - 1
+            infoAuxiliary(iSubband + nSubbands) = trace(channelBlkDiag{iSubband + nSubbands} * infoMatrix);
+            powerAuxiliary(iSubband + nSubbands) = trace(channelBlkDiag{iSubband + nSubbands} * powerMatrix);
+        end
+
+        % * Compute current
+        currentCandidate = (1 / 2) * beta2 * (infoAuxiliary(nSubbands) + powerAuxiliary(nSubbands)) ...
+            + (3 / 8) * beta4 ^ 2 * (2 * infoAuxiliary(nSubbands) ^ 2 + (powerAuxiliary' * powerAuxiliary)) ...
+            + (3 / 2) * beta4 ^ 2 * infoAuxiliary(nSubbands) * powerAuxiliary(nSubbands);
+
+        % * Choose best candidate
+        if currentCandidate >= current
+            current = currentCandidate;
+            infoAmplitude = transpose(infoAmplitudeCandidate);
+            powerAmplitude = transpose(powerAmplitudeCandidate);
+        end
+    end
+
+    % * Reconstruct waveform by power allocation + beamforming
+    infoWaveform = infoAmplitude .* channel' ./ vecnorm(channel, 2, 2)';
+    powerWaveform = powerAmplitude .* channel' ./ vecnorm(channel, 2, 2)';
+
+end
