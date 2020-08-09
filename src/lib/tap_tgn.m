b435f0bd61b5671fc3a3ef4211ce1555368fa77f
SnowzTail <i@snowztail.com>
2020-08-09 10:58:26 +0100

    :art: Generate taps with predefined variables

--- src/lib/tap_tgn.m
+++ src/lib/tap_tgn.m
@@ -1,20 +1,24 @@
-function [tapGain, tapDelay] = tap_tgn(nTxs, nRxs, propagationMode)
+function [tapGain, tapDelay] = tap_tgn(corTx, corRx, losMatrix, variable, propagationMode)
     % Function:
     %   - simulate channel using the power delay profile of the IEEE TGn NLOS channel model E
     %
     % Input:
-    %   - nTxs (M): number of transmit antennas
-    %   - nRxs: number of receive antennas
+    %   - corTx (R_t) [nTxs * nTxs]: transmit correlation matrix
+    %   - corRx (R_r) [nRxs * nRxs]: receive correlation matrix
+    %   - losMatrix [nRxs * nTxs]: fixed LOS response matrix (entries with unit modulus)
+    %   - variable {nClusters * nTaps}: i.i.d. CSCG variables as uncorrelated tap response
+    %   - propagationMode: propagration mode ('los' or 'nlos')
     %
     % Output:
     %   - tapGain [nTaps * nTxs * nRxs]: complex tap gain
     %   - tapDelay [nTaps * 1]: tap delays
     %
     % Comment:
-    %   - for single-user MIMO
+    %   - for single-user MIMO, consider spatial correlation
     %   - only use power delay profile of clusters in the reference (path loss redefined)
-    %   - for each tap, model LOS component as variable with unit modulus and uniform phase distribution, and NLOS component as CSCG variable
+    %   - for each tap, the LOS component is fixed while the NLOS component consists of spatially correlated variables
     %   - LOS Ricean factor only apply to the first LOS tap, the remaining taps use NLOS Ricean factor
+    %   - LOS matrix depends on geometry position
     %
     % Reference:
     %   - V. Erceg et al., "TGn channel models," in Version 4. IEEE 802.11–03/940r4, May 2004.
@@ -23,6 +27,8 @@ function [tapGain, tapDelay] = tap_tgn(nTxs, nRxs, propagationMode)
 
 
     % * Define parameters
+    nTxs = size(corTx, 1);
+    nRxs = size(corRx, 1);
     nClusters = 4;
     nTaps = 18;
     tapDelay = 1e-9 * [0 10 20 30 50 80 110 140 180 230 280 330 380 430 490 560 640 730]';
@@ -34,17 +40,23 @@ function [tapGain, tapDelay] = tap_tgn(nTxs, nRxs, propagationMode)
     losRiceanFactor = db2pow(6);
     nlosRiceanFactor = db2pow(-inf);
 
-    % * Assign Ricean factor according to propagation mode
-    riceanFactor = repmat(nlosRiceanFactor, [nClusters, nTaps, nTxs, nRxs]);
-    if propagationMode == "los"
-        riceanFactor(:, 1, :, :) = losRiceanFactor;
+    % * Generate tap gains and sum over clusters
+    losGain = zeros(nClusters, nTaps, nRxs, nTxs);
+    nlosGain = zeros(nClusters, nTaps, nRxs, nTxs);
+    tapGain = zeros(nClusters, nTaps, nRxs, nTxs);
+    for iCluster = 1 : nClusters
+        for iTap = 1 : nTaps
+            if iTap == 1 && propagationMode == "los"
+                losGain(iCluster, iTap, :, :) = sqrt(losRiceanFactor / (losRiceanFactor + 1)) * losMatrix;
+            else
+                losGain(iCluster, iTap, :, :) = sqrt(nlosRiceanFactor / (nlosRiceanFactor + 1)) * losMatrix;
+            end
+            nlosGain(iCluster, iTap, :, :) = sqrt(1 / (nlosRiceanFactor + 1)) * (corRx ^ (1 / 2) * sqrt(1 / 2) * variable{iCluster, iTap}(1 : nRxs, 1 : nTxs) * transpose(corTx ^ (1 / 2)));
+            tapGain(iCluster, iTap, :, :) = sqrt(tapPower(iCluster, iTap)) * (losGain(iCluster, iTap, :, :) + nlosGain(iCluster, iTap, :, :));
+        end
     end
 
-    % * Generate taps and sum over clusters
-    losGain = sqrt(riceanFactor ./ (riceanFactor + 1)) .* exp(1i * 2 * pi * rand(nClusters, nTaps, nTxs, nRxs));
-    nlosGain = sqrt(1 ./ (riceanFactor + 1)) .* (randn(nClusters, nTaps, nTxs, nRxs) + 1i * randn(nClusters, nTaps, nTxs, nRxs));
-    tapGain = repmat(sqrt(tapPower / 2), [1 1 nTxs nRxs]) .* (losGain + nlosGain);
-    tapGain = sum(tapGain, 1);
-    tapGain = permute(tapGain, [2 3 4 1]);
+    % * Sum over clusters and reshape to [nTaps, nTxs, nRxs]
+    tapGain = permute(sum(tapGain, 1), [2 4 3 1]);
 
 end
--- src/tap_response.m
+++ src/tap_response.m
@@ -1,19 +1,27 @@
 clear; clc; setup;
 
-%% * Transceiver
-% number of transmit and receive antennas
+%% * Dataset parameters
+% max number of transmit antennas
 nTxs = 10;
+% number of receive antenna
 nRxs = 1;
 % max number of reflecting elements in IRS
 nReflectors = 100;
+% number of clusters in channel model
+nClusters = 4;
+% number of taps in channel model
+nTaps = 18;
 
-%% * NLOS tap response
-[directTapGain, directTapDelay] = tap_tgn(nTxs, nRxs, 'nlos');
-[incidentTapGain, incidentTapDelay] = tap_tgn(nTxs, nReflectors, 'nlos');
-[reflectiveTapGain, reflectiveTapDelay] = tap_tgn(nReflectors, nRxs, 'nlos');
-save('data/tap_nlos.mat', 'directTapGain', 'directTapDelay', 'incidentTapGain', 'incidentTapDelay', 'reflectiveTapGain', 'reflectiveTapDelay');
+%% * Generate CSCG variables as uncorrelated tap response
+directVariable = cell(nClusters, nTaps);
+incidentVariable = cell(nClusters, nTaps);
+reflectiveVariable = cell(nClusters, nTaps);
+for iCluster = 1 : nClusters
+    for iTap = 1 : nTaps
+        directVariable{iCluster, iTap} = sqrt(1 / 2) * (randn(nRxs, nTxs) + 1i * randn(nRxs, nTxs));
+        incidentVariable{iCluster, iTap} = sqrt(1 / 2) * (randn(nReflectors, nTxs) + 1i * randn(nReflectors, nTxs));
+        reflectiveVariable{iCluster, iTap} = sqrt(1 / 2) * (randn(nRxs, nReflectors) + 1i * randn(nRxs, nReflectors));
+    end
+end
 
-%% * LOS tap response
-[incidentTapGain, incidentTapDelay] = tap_tgn(nTxs, nReflectors, 'los');
-[reflectiveTapGain, reflectiveTapDelay] = tap_tgn(nReflectors, nRxs, 'los');
-save('data/tap_los.mat', 'directTapGain', 'directTapDelay', 'incidentTapGain', 'incidentTapDelay', 'reflectiveTapGain', 'reflectiveTapDelay');
+save('data/variable.mat', 'directVariable', 'incidentVariable', 'reflectiveVariable');
