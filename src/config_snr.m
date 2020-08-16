db97bec48dc1fef21478d2892fee9ca1d52b38f8
SnowzTail <i@snowztail.com>
2020-08-16 17:36:10 +0100

    :sparkles: R-E region vs SNR

---
+++ src/config_snr.m
@@ -0,0 +1,60 @@
+%% * Transceiver
+% diode k-parameter
+k2 = 0.0034;
+k4 = 0.3829;
+% antenna resistance
+resistance = 50;
+% coefficients on current terms
+beta2 = k2 * resistance;
+beta4 = k4 * resistance ^ 2;
+% number of transmit and receive antennas
+nTxs = 1;
+nRxs = 1;
+% number of users
+nUsers = 1;
+% average transmit power
+txPower = db2pow(6);
+% receive antenna gain
+rxGain = db2pow(2);
+
+%% * Channel
+% AP-user distance
+directDistance = 15;
+% vertical distance from the IRS to the AP-user path
+verticalDistance = 2;
+% projection of AP-IRS distance to the AP-user path
+horizontalDistance = 2;
+% AP-IRS and IRS-user distance
+[incidentDistance, reflectiveDistance] = coordinate(directDistance, verticalDistance, horizontalDistance);
+% center frequency
+centerFrequency = 5.18e9;
+% bandwidth
+bandwidth = 1e6;
+% number of frequency bands
+nSubbands = 4;
+% channel fading mode ('flat' or 'selective')
+fadingMode = 'selective';
+% carrier frequency
+[subbandFrequency] = subband_frequency(centerFrequency, bandwidth, nSubbands);
+% number of reflecting elements in IRS
+nReflectors = 20;
+% spatial correlation
+corTx = eye(nTxs);
+corRx = eye(nRxs);
+corIrs = eye(nReflectors);
+
+%% * Algorithm
+% minimum gain per iteration
+tolerance = 1e-8;
+% number of CSCG random vectors to generate
+nCandidates = 1e3;
+% number of samples in R-E curves
+nSamples = 40;
+% number of channel realizations
+nChannels = 1e2;
+% number of cases to investigate
+nCases = 2;
+
+%% * Variable
+% large-scale signal-to-noise ratio
+Variable.snr = db2pow(10 : 10 : 40);
---
+++ src/re_snr.m
@@ -0,0 +1,51 @@
+clear; clc; setup; config_snr;
+
+%% ! R-E region for different large-scale SNR
+reSample = cell(nChannels, length(Variable.snr));
+reSolution = cell(nChannels, length(Variable.snr));
+
+for iChannel = 1 : nChannels
+    % * Generate tap gains and delays
+    [directTapGain, directTapDelay] = tap_tgn(corTx, corRx, 'nlos');
+    [incidentTapGain, incidentTapDelay] = tap_tgn(corTx, corIrs, 'nlos');
+    [reflectiveTapGain, reflectiveTapDelay] = tap_tgn(corIrs, corRx, 'nlos');
+
+    % * Construct channels
+    [directChannel] = frequency_response(directTapGain, directTapDelay, directDistance, rxGain, subbandFrequency, fadingMode);
+    [incidentChannel] = frequency_response(incidentTapGain, incidentTapDelay, incidentDistance, rxGain, subbandFrequency, fadingMode);
+    [reflectiveChannel] = frequency_response(reflectiveTapGain, reflectiveTapDelay, reflectiveDistance, rxGain, subbandFrequency, fadingMode);
+
+    % * Calculate sum pathloss
+    [sumPathloss] = sum_pathloss(directDistance, incidentDistance, reflectiveDistance);
+
+    for iSnr = 1 : length(Variable.snr)
+        % * Calculate noise power based on SNR
+        noisePower = txPower * sumPathloss * rxGain / Variable.snr(iSnr);
+
+        % * Alternating optimization
+        [reSample{iChannel, iSnr}, reSolution{iChannel, iSnr}] = re_sample(beta2, beta4, directChannel, incidentChannel, reflectiveChannel, txPower, noisePower, nCandidates, nSamples, tolerance);
+    end
+end
+
+% * Average over channel realizations
+reSampleAvg = cell(1, length(Variable.snr));
+for iSnr = 1 : length(Variable.snr)
+    reSampleAvg{iSnr} = mean(cat(3, reSample{:, iSnr}), 3);
+end
+save('data/re_snr.mat');
+
+%% * R-E plots
+figure('name', 'R-E region vs large-scale SNR');
+legendString = cell(length(Variable.snr), 1);
+for iSnr = 1 : length(Variable.snr)
+    plot(reSampleAvg{iSnr}(1, :) / nSubbands, 1e6 * reSampleAvg{iSnr}(2, :));
+    legendString{iSnr} = sprintf('SNR = %d dB', pow2db(Variable.snr(iSnr)));
+    hold on;
+end
+hold off;
+grid minor;
+legend(legendString);
+xlabel('Per-subband rate [bps/Hz]');
+ylabel('Average output DC current [\muA]');
+ylim([0 inf]);
+savefig('plots/re_snr.fig');
