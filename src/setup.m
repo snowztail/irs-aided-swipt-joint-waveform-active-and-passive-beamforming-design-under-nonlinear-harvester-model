0a1cacea6f4ca577b134721461b6bd6e5e255c36
SnowzTail <i@snowztail.com>
2020-08-30 23:48:21 +0100

    :cloud: HPC config

--- src/setup.m
+++ src/setup.m
@@ -3,5 +3,5 @@ run('/rds/general/user/yz16718/home/code/cvx/cvx_setup.m');
 
 nCpus = str2num(getenv('NCPUS'));
 parpool(nCpus);
-% maxNumCompThreads(nCpus);
+maxNumCompThreads(nCpus);
 feature('numcores');
