af8db21fee994981ef6154f678e987aab06ea241
SnowzTail <i@snowztail.com>
2020-09-18 12:40:25 +0100

    :wrench: Update config

--- src/setup.m
+++ src/setup.m
@@ -7,7 +7,7 @@ set(groot, 'defaultlegendinterpreter', 'latex');
 set(groot, 'defaultlinelinewidth', 2);
 set(groot, 'defaultstemlinewidth', 2);
 
-run('/rds/general/user/yz16718/home/code/cvx/cvx_setup.m');
+run('/rds/general/user/yz16718/home/cvx/cvx_setup.m');
 
 iBatch = str2double(getenv('PBS_ARRAY_INDEX'));
 disp(iBatch);
