e874fd299a80fcc8f09da9f96ae89ca085a3f40c
SnowzTail <i@snowztail.com>
2020-09-21 16:19:12 +0100

    :sparkles: R-E region vs splitting ratio

--- src/plot/plot_noise.m
+++ src/plot/plot_noise.m
@@ -1,12 +1,20 @@
 clear; clc; close all; config_noise;
 
 %% * Load batch data
+indexSet = 1 : nSamples;
 reSet = cell(nBatches, length(Variable.noisePower));
+powerRatio = zeros(nBatches, length(Variable.noisePower), nSamples);
 for iBatch = 1 : nBatches
     try
-        load(sprintf('../data/re_noise/re_noise_%d.mat', iBatch), 'reInstance');
+        load(sprintf('../data/re_noise/re_noise_%d.mat', iBatch), 'reInstance', 'reSolution');
         reSet(iBatch, :) = reInstance;
-    catch
+		for iNoise = 1 : length(Variable.noisePower)
+			for iSample = 1 : nSamples
+				powerRatio(iBatch, iNoise, iSample) = reSolution{iNoise}{iSample}.powerRatio;
+			end
+		end
+	catch
+		indexSet(indexSet == iBatch) = [];
         disp(iBatch);
     end
 end
@@ -14,12 +22,17 @@ end
 %% * Average over batches
 reNoise = cell(1, length(Variable.noisePower));
 for iNoise = 1 : length(Variable.noisePower)
-    reNoise{iNoise} = mean(cat(3, reSet{:, iNoise}), 3);
+    reNoise{iNoise} = mean(cat(3, reSet{indexSet, iNoise}), 3);
 end
+powerRatio = permute(mean(powerRatio(indexSet, :, :), 1), [2 3 1]);
 save('../data/re_noise.mat');
 
-%% * R-E plots
-figure('name', 'R-E region vs average noise power');
+%% * R-E and splitting ratio plots
+figure('name', 'R-E region and power splitting ratio vs average noise power');
+tiledlayout(2, 1, 'tilespacing', 'compact');
+
+% * R-E region
+nexttile;
 legendString = cell(1, length(Variable.noisePower));
 plotHandle = gobjects(1, length(Variable.noisePower));
 for iNoise = 1 : length(Variable.noisePower)
@@ -34,7 +47,22 @@ xlabel('Per-subband rate [bps/Hz]');
 ylabel('Average output DC current [$\mu$A]');
 xlim([0 inf]);
 ylim([0 inf]);
-
 apply_style(plotHandle);
+
+% * Power splitting ratio
+nexttile;
+plotHandle = gobjects(1, length(Variable.noisePower));
+for iNoise = 1 : length(Variable.noisePower)
+    plotHandle(iNoise) = plot(reNoise{iNoise}(1, :) / nSubbands, powerRatio(iNoise, :));
+    hold on;
+end
+hold off;
+grid on;
+xlabel('Per-subband rate [bps/Hz]');
+ylabel('Power splitting ratio');
+xlim([0 inf]);
+ylim([0 inf]);
+apply_style(plotHandle);
+
 savefig('../figures/re_noise.fig');
 matlab2tikz('../../assets/re_noise.tex');
