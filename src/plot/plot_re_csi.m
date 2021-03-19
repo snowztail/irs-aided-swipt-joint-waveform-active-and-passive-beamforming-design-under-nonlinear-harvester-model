ce86183f12b1f98f20a4cebbdec990586ec7ff2e
SnowzTail <i@snowztail.com>
2021-03-19 17:33:57 +0000

    :construction: Imperfect vs random IRS

--- src/plot/plot_re_csi.m
+++ src/plot/plot_re_csi.m
@@ -2,13 +2,13 @@ clear; clc; close all; config_re_csi;
 
 %% * Load batch data
 indexSet = 1 : nBatches;
-reNoIrsSet = cell(nBatches, 1);
-reErrorSet = cell(nBatches, length(Variable.cascadedErrorVariance));
+reRandomSet = cell(nBatches, length(Variable.nSubbands));
+reErrorSet = cell(nBatches, length(Variable.nSubbands), length(Variable.cascadedErrorVariance));
 for iBatch = 1 : nBatches
     try
-        load(sprintf('../data/re_csi/re_csi_%d.mat', iBatch), 'reNoIrsInstance', 'reErrorInstance');
-        reNoIrsSet{iBatch} = reNoIrsInstance;
-        reErrorSet(iBatch, :) = reErrorInstance;
+        load(sprintf('../data/re_csi/re_csi_%d.mat', iBatch), 'reRandomInstance', 'reErrorInstance');
+        reRandomSet(iBatch, :) = reRandomInstance;
+        reErrorSet(iBatch, :, :) = reErrorInstance;
     catch
 		indexSet(indexSet == iBatch) = [];
         disp(iBatch);
@@ -16,33 +16,39 @@ for iBatch = 1 : nBatches
 end
 
 %% * Average over batches
-reNoIrsCsi = mean(cat(3, reNoIrsSet{indexSet}), 3);
-reErrorCsi = cell(1, length(Variable.cascadedErrorVariance));
-for iError = 1 : length(Variable.cascadedErrorVariance)
-    reErrorCsi{iError} = mean(cat(3, reErrorSet{indexSet, iError}), 3);
+reRandomCsi = cell(length(Variable.nSubbands), 1);
+reErrorCsi = cell(length(Variable.nSubbands), length(Variable.cascadedErrorVariance));
+for iSubband = 1 : length(Variable.nSubbands)
+	reRandomCsi{iSubband} = mean(cat(3, reRandomSet{indexSet, iSubband}), 3);
+	for iError = 1 : length(Variable.cascadedErrorVariance)
+		reErrorCsi{iSubband, iError} = mean(cat(4, reErrorSet{indexSet, iSubband, iError}), 4);
+	end
 end
 save('../data/re_csi.mat');
 
 %% * R-E plots
 figure('name', 'R-E region vs cascaded channel estimation error', 'position', [0, 0, 500, 400]);
-legendString = cell(1, length(Variable.cascadedErrorVariance) + 1);
-plotHandle = gobjects(1, length(Variable.cascadedErrorVariance) + 1);
+legendString = cell(length(Variable.nSubbands), length(Variable.cascadedErrorVariance) + 1);
+plotHandle = gobjects(length(Variable.nSubbands), length(Variable.cascadedErrorVariance) + 1);
 hold all;
-for iError = 1 : length(Variable.cascadedErrorVariance)
-    plotHandle(iError) = plot(reErrorCsi{iError}(1, :) / nSubbands, 1e6 * reErrorCsi{iError}(2, :));
-	legendString{iError} = sprintf('$\\epsilon_n^2 = %s \\Lambda_I\\Lambda_R$', num2str(Variable.cascadedErrorVarianceRatio(iError)));
+for iSubband = 1 : length(Variable.nSubbands)
+	nSubbands = Variable.nSubbands(iSubband);
+	for iError = 1 : length(Variable.cascadedErrorVariance)
+		plotHandle(iSubband, iError) = plot(reErrorCsi{iSubband, iError}(1, :) / nSubbands, 1e6 * reErrorCsi{iSubband, iError}(2, :));
+		legendString{iSubband, iError} = sprintf('$\\epsilon_n^2 = %s \\Lambda_I\\Lambda_R (N = %s)$', num2str(Variable.cascadedErrorVarianceRatio(iError)), num2str(Variable.nSubbands(iSubband)));
+	end
+	plotHandle(iSubband, iError + 1) = plot(reRandomCsi{iSubband}(1, :) / nSubbands, 1e6 * reRandomCsi{iSubband}(2, :));
+	legendString{iSubband, iError + 1} = sprintf('Random IRS $(N = %s)$', num2str(Variable.nSubbands(iSubband)));
 end
-plotHandle(end) = plot(reNoIrsCsi(1, :) / nSubbands, 1e6 * reNoIrsCsi(2, :));
-legendString{end} = 'No IRS';
 hold off;
 grid on;
-legend(legendString);
+legend(vec(transpose(legendString)));
 xlabel('Per-subband rate [bps/Hz]');
 ylabel('Output DC current [$\mu$A]');
 xlim([0 inf]);
 ylim([0 inf]);
 box on;
-apply_style(plotHandle);
+apply_group_style(vec(transpose(plotHandle)), length(Variable.cascadedErrorVariance) + 1);
 
 savefig('../figures/re_csi.fig');
 matlab2tikz('../../assets/re_csi.tex', 'extraaxisoptions', ['title style={font=\huge}, ' 'label style={font=\huge}, ' 'ticklabel style={font=\LARGE}, ' 'legend style={font=\LARGE}']);
--- src/re_csi.m
+++ src/re_csi.m
@@ -1,11 +1,11 @@
 clear; clc; setup; config_re_csi;
 
 %% ! R-E region vs CSIT error
-reNoIrsSample = cell(nChannels, 1);
-reErrorSample = cell(nChannels, length(Variable.cascadedErrorVariance));
+reRandomSample = cell(nChannels, length(Variable.nSubbands));
+reErrorSample = cell(nChannels, length(Variable.nSubbands), length(Variable.cascadedErrorVariance));
 
-reNoIrsSolution = cell(nChannels, 1);
-reErrorSolution = cell(nChannels, length(Variable.cascadedErrorVariance));
+reRandomSolution = cell(nChannels, length(Variable.nSubbands));
+reErrorSolution = cell(nChannels, length(Variable.nSubbands), length(Variable.cascadedErrorVariance));
 
 for iChannel = 1 : nChannels
 	% * Generate tap gains and delays
@@ -13,32 +13,43 @@ for iChannel = 1 : nChannels
 	[incidentTapGain, incidentTapDelay] = tap_tgn(corTx, corIrs, 'nlos');
 	[reflectiveTapGain, reflectiveTapDelay] = tap_tgn(corIrs, corRx, 'nlos');
 
-	% * Construct channels
-	[directChannel] = channel_response(directTapGain, directTapDelay, directDistance, rxGain, subbandFrequency, fadingMode);
-	[incidentChannel] = channel_response(incidentTapGain, incidentTapDelay, incidentDistance, irsGain, subbandFrequency, fadingMode);
-	[reflectiveChannel] = channel_response(reflectiveTapGain, reflectiveTapDelay, reflectiveDistance, rxGain, subbandFrequency, fadingMode);
-	[cascadedChannel] = cascaded_channel(incidentChannel, reflectiveChannel);
+	for iSubband = 1 : length(Variable.nSubbands)
+        % * Get number of subbands and subband frequency
+        nSubbands = Variable.nSubbands(iSubband);
+        [subbandFrequency] = subband_frequency(centerFrequency, bandwidth, nSubbands);
 
-	% * R-E region without IRS
-	[reNoIrsSample{iChannel}, reNoIrsSolution{iChannel}] = re_sample_swipt_gp_benchmark(alpha, beta2, beta4, directChannel, txPower, noisePower, nSamples, tolerance);
+		% * Construct channels
+		[directChannel] = channel_response(directTapGain, directTapDelay, directDistance, rxGain, subbandFrequency, fadingMode);
+		[incidentChannel] = channel_response(incidentTapGain, incidentTapDelay, incidentDistance, irsGain, subbandFrequency, fadingMode);
+		[reflectiveChannel] = channel_response(reflectiveTapGain, reflectiveTapDelay, reflectiveDistance, rxGain, subbandFrequency, fadingMode);
+		[cascadedChannel] = cascaded_channel(incidentChannel, reflectiveChannel);
 
-    for iError = 1 : length(Variable.cascadedErrorVariance)
-		% * Update error variance of the cascaded channel
-        cascadedErrorVariance = Variable.cascadedErrorVariance(iError);
+		% * R-E region with random IRS
+		randomIrs = exp(1i * 2 * pi * rand(nReflectors, 1));
+		randomCompositeChannel = composite_channel(directChannel, cascadedChannel, randomIrs);
+		[reRandomSample{iChannel, iSubband}, reRandomSolution{iChannel, iSubband}] = re_sample_swipt_gp_benchmark(alpha, beta2, beta4, randomCompositeChannel, txPower, noisePower, nSamples, tolerance);
 
-		% * Emulate cascaded channel estimation
-		[imperfectCascadedChannel] = imperfect_csi(cascadedChannel, cascadedErrorVariance);
+		for iError = 1 : length(Variable.cascadedErrorVariance)
+			% * Update error variance of the cascaded channel
+			cascadedErrorVariance = Variable.cascadedErrorVariance(iError);
 
-        % * R-E region based on imperfect cascaded CSIT
-        [reErrorSample{iChannel, iError}, reErrorSolution{iChannel, iError}] = re_sample_swipt_imperfect_csi(alpha, beta2, beta4, directChannel, cascadedChannel, imperfectCascadedChannel, txPower, noisePower, nCandidates, nSamples, tolerance);
-    end
+			% * Emulate cascaded channel estimation
+			[imperfectCascadedChannel] = imperfect_csi(cascadedChannel, cascadedErrorVariance);
+
+			% * R-E region based on imperfect cascaded CSIT
+			[reErrorSample{iChannel, iSubband, iError}, reErrorSolution{iChannel, iSubband, iError}] = re_sample_swipt_imperfect_csi(alpha, beta2, beta4, directChannel, cascadedChannel, imperfectCascadedChannel, txPower, noisePower, nCandidates, nSamples, tolerance);
+		end
+	end
 end
 
 % * Average over channel realizations
-reNoIrsInstance = mean(cat(3, reNoIrsSample{:}), 3);
-reErrorInstance = cell(1, length(Variable.cascadedErrorVariance));
-for iError = 1 : length(Variable.cascadedErrorVariance)
-    reErrorInstance{iError} = mean(cat(3, reErrorSample{:, iError}), 3);
+reRandomInstance = cell(length(Variable.nSubbands), 1);
+reErrorInstance = cell(length(Variable.nSubbands), length(Variable.cascadedErrorVariance));
+for iSubband = 1 : length(Variable.nSubbands)
+	reRandomInstance{iSubband} = mean(cat(3, reRandomSample{:, iSubband}), 3);
+	for iError = 1 : length(Variable.cascadedErrorVariance)
+		reErrorInstance{iSubband, iError} = mean(cat(4, reErrorSample{:, iSubband, iError}), 4);
+	end
 end
 
 % * Save batch data
