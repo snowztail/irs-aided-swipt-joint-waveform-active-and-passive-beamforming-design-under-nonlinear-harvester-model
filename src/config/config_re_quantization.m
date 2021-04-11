54efa3462422f398e5498df0a65ae1027ccf0ca9
SnowzTail <i@snowztail.com>
2021-04-11 21:55:08 +0100

    :wrench: Update config

--- src/config/config_re_quantization.m
+++ src/config/config_re_quantization.m
@@ -35,7 +35,7 @@ horizontalDistance = 2;
 % center frequency
 centerFrequency = 5.18e9;
 % bandwidth
-bandwidth = 1e6;
+bandwidth = 1e7;
 % number of frequency bands
 nSubbands = 16;
 % channel fading mode ('flat' or 'selective')
@@ -61,7 +61,7 @@ nChannels = 1;
 
 %% * Variables
 % number of bits used for quantization
-Variable.nQuantizeBits = 2 .^ (0 : 3);
+Variable.nQuantizeBits = 2 .^ (0 : 2);
 
 %% * PBS
 % number of individual jobs
