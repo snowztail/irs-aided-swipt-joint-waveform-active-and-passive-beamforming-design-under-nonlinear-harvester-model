3902d44903be24564efe1b8adcde94b0bd2e8f54
SnowzTail <i@snowztail.com>
2021-07-26 02:30:45 +0100

    :hammer: Update style function

--- src/tools/apply_group_style.m
+++ src/tools/apply_group_style.m
@@ -2,5 +2,5 @@ function apply_group_style(h, g)
 	c = {'#0072BD'; '#D95319'; '#EDB120'; '#7E2F8E'; '#77AC30'; '#4DBEEE'; '#A2142F'};
 	l = {'-'; '--'; ':'; '-.'};
 	m = {'o'; '+'; 's'; 'x'; '^'; 'v'; '>'; '<'};
-	set(h, {'color'}, c(rem(repelem((1 : numel(h) / g) - 1, g), numel(c)) + 1),	{'linestyle'}, l(rem(repmat((1 : g) - 1, [1, numel(h) / g]), numel(l)) + 1), {'marker'}, m(rem((1 : numel(h)) - 1, numel(m)) + 1));
+	set(h, {'color'}, c(rem(repelem((1 : numel(h) / g) - 1, g), numel(c)) + 1),	{'linestyle'}, l(rem(repmat((1 : g) - 1, [1, numel(h) / g]), numel(l)) + 1), {'marker'}, m(rem(repelem((1 : numel(h) / g) - 1, g), numel(m)) + 1));
 end
