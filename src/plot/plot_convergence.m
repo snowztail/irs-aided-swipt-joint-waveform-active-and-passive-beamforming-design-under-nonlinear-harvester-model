bd683ed3cab9f703ed60316d822fb33f76ff4822
SnowzTail <i@snowztail.com>
2021-07-27 04:24:21 +0100

    :paintbrush: Add convergence plots and update IRS plots

---
+++ src/plot/plot_convergence.m
@@ -0,0 +1,100 @@
+clear; clc; close all; config_convergence;
+
+%% * Retrieve data
+% * Load data and select one point with medium rate and current
+load('../data/convergence.mat');
+sampleIndex = ceil(nSamples / 2);
+
+% * Algorithm 1
+scaIter = aoSolution{sampleIndex}.scaIter{1}(2, :);
+scaIter(end : nIters) = scaIter(end);
+
+% * Algorithm 2
+gpIter = aoSolution{sampleIndex}.gpIter{1}(2, :);
+gpIter(end : nIters) = gpIter(end);
+
+% * Algorithm 3
+mScaIter = lcSolution{sampleIndex}.mScaIter{1}(2, :);
+mScaIter(end : nIters) = mScaIter(end);
+
+% * Algorithm 4
+bcdIter = aoSolution{sampleIndex}.bcdIter(2, :);
+bcdIter(end : nIters) = bcdIter(end);
+
+% * Algorithm 5
+lcBcdIter = lcSolution{sampleIndex}.lcBcdIter(2, :);
+lcBcdIter(end : nIters) = lcBcdIter(end);
+
+%% * Convergence trends of SCA-based algorithms
+figure('name', 'Convergence trends of SCA-based algorithms', 'position', [0, 0, 500, 400]);
+scaPlot = tiledlayout(2, 1, 'tilespacing', 'compact');
+
+% * Algorithm 1
+nexttile;
+plot(1e6 * scaIter, 'color', '#0072BD', 'linestyle', '-', 'marker', 'o');
+grid on;
+legend('SCA', 'location', 'se');
+ylabel('DC current [$\mu$A]');
+xlim([1, nIters]);
+xticks([1, 5 : 5 : nIters]);
+box on;
+
+% * Algorithm 3
+nexttile;
+plot(1e6 * mScaIter, 'color', '#D95319', 'linestyle', '--', 'marker', '+');
+grid on;
+legend('M-SCA', 'location', 'se');
+xlabel('Iteration index');
+ylabel('DC current [$\mu$A]');
+xlim([1, nIters]);
+xticks([1, 5 : 5 : nIters]);
+box on;
+
+savefig('../figures/convergence_sca.fig');
+matlab2tikz('../../assets/convergence_sca.tex', 'extraaxisoptions', ['title style={font=\huge}, ' 'label style={font=\huge}, ' 'ticklabel style={font=\LARGE}, ' 'legend style={font=\LARGE}']);
+close;
+
+%% * Convergence trends of GP-based algorithm
+figure('name', 'Convergence trends of GP-based algorithm', 'position', [0, 0, 500, 400]);
+
+% * Algorithm 2
+plot(1e6 * gpIter, 'color', '#0072BD', 'linestyle', '-', 'marker', 'o');
+grid on;
+legend('GP', 'location', 'se');
+xlabel('Iteration index');
+ylabel('DC current [$\mu$A]');
+xlim([1, nIters]);
+xticks([1, 5 : 5 : nIters]);
+box on;
+
+savefig('../figures/convergence_gp.fig');
+matlab2tikz('../../assets/convergence_gp.tex', 'extraaxisoptions', ['title style={font=\huge}, ' 'label style={font=\huge}, ' 'ticklabel style={font=\LARGE}, ' 'legend style={font=\LARGE}']);
+close;
+
+%% * Convergence trends of BCD-based algorithms
+figure('name', 'Convergence trends of BCD-based algorithms', 'position', [0, 0, 500, 400]);
+bcdPlot = tiledlayout(2, 1, 'tilespacing', 'compact');
+
+% * Algorithm 4
+nexttile;
+plot(1e6 * bcdIter, 'color', '#0072BD', 'linestyle', '-', 'marker', 'o');
+grid on;
+legend('BCD', 'location', 'se');
+ylabel('DC current [$\mu$A]');
+xlim([1, nIters]);
+xticks([1, 5 : 5 : nIters]);
+box on;
+
+% * Algorithm 5
+nexttile;
+plot(1e6 * lcBcdIter, 'color', '#D95319', 'linestyle', '--', 'marker', '+');
+grid on;
+legend('LC-BCD', 'location', 'se');
+xlabel('Iteration index');
+ylabel('DC current [$\mu$A]');
+xlim([1, nIters]);
+xticks([1, 5 : 5 : nIters]);
+box on;
+
+savefig('../figures/convergence_bcd.fig');
+matlab2tikz('../../assets/convergence_bcd.tex', 'extraaxisoptions', ['title style={font=\huge}, ' 'label style={font=\huge}, ' 'ticklabel style={font=\LARGE}, ' 'legend style={font=\LARGE}']);
--- src/plot/plot_re_irs.m
+++ src/plot/plot_re_irs.m
@@ -4,15 +4,17 @@ clear; clc; close all; config_re_irs;
 indexSet = 1 : nBatches;
 reIdealIrsSet = cell(nBatches, length(Variable.bandwidth));
 reAdaptiveIrsSet = cell(nBatches, length(Variable.bandwidth));
+reLinearIrsSet = cell(nBatches, length(Variable.bandwidth));
 reWitIrsSet = cell(nBatches, length(Variable.bandwidth));
 reWptIrsSet = cell(nBatches, length(Variable.bandwidth));
 reRandomIrsSet = cell(nBatches, length(Variable.bandwidth));
 reNoIrsSet = cell(nBatches, length(Variable.bandwidth));
 for iBatch = 1 : nBatches
     try
-		load(sprintf('../data/re_irs/re_irs_%d.mat', iBatch), 'reIdealIrsInstance', 'reAdaptiveIrsInstance', 'reWitIrsInstance', 'reWptIrsInstance', 'reRandomIrsInstance', 'reNoIrsInstance');
+		load(sprintf('../data/re_irs/re_irs_%d.mat', iBatch), 'reIdealIrsInstance', 'reAdaptiveIrsInstance', 'reLinearIrsInstance', 'reWitIrsInstance', 'reWptIrsInstance', 'reRandomIrsInstance', 'reNoIrsInstance');
         reIdealIrsSet(iBatch, :) = reIdealIrsInstance;
         reAdaptiveIrsSet(iBatch, :) = reAdaptiveIrsInstance;
+        reLinearIrsSet(iBatch, :) = reLinearIrsInstance;
         reWitIrsSet(iBatch, :) = reWitIrsInstance;
         reWptIrsSet(iBatch, :) = reWptIrsInstance;
         reRandomIrsSet(iBatch, :) = reRandomIrsInstance;
@@ -26,6 +28,7 @@ end
 %% * Average over batches
 reIdealIrs = cell(1, length(Variable.bandwidth));
 reAdaptiveIrs = cell(1, length(Variable.bandwidth));
+reLinearIrs = cell(1, length(Variable.bandwidth));
 reWitIrs = cell(1, length(Variable.bandwidth));
 reWptIrs = cell(1, length(Variable.bandwidth));
 reRandomIrs = cell(1, length(Variable.bandwidth));
@@ -33,6 +36,7 @@ reNoIrs = cell(1, length(Variable.bandwidth));
 for iBandwidth = 1 : length(Variable.bandwidth)
 	reIdealIrs{iBandwidth} = mean(cat(3, reIdealIrsSet{indexSet, iBandwidth}), 3);
 	reAdaptiveIrs{iBandwidth} = mean(cat(3, reAdaptiveIrsSet{indexSet, iBandwidth}), 3);
+	reLinearIrs{iBandwidth} = mean(cat(3, reLinearIrsSet{indexSet, iBandwidth}), 3);
 	reWitIrs{iBandwidth} = mean(cat(3, reWitIrsSet{indexSet, iBandwidth}), 3);
 	reWptIrs{iBandwidth} = mean(cat(3, reWptIrsSet{indexSet, iBandwidth}), 3);
 	reRandomIrs{iBandwidth} = mean(cat(3, reRandomIrsSet{indexSet, iBandwidth}), 3);
@@ -42,18 +46,19 @@ save('../data/re_irs.mat');
 
 %% * R-E plots
 for iBandwidth = 1 : length(Variable.bandwidth)
-	figure('name', sprintf('R-E region for ideal, adaptive, nonadaptive, random and no IRS for $B = %d$ MHz', Variable.bandwidth(iBandwidth) / 1e6), 'position', [0, 0, 500, 400]);
+	figure('name', sprintf('R-E region for ideal, adaptive (linear and nonlinear EH model), nonadaptive, random and no IRS for $B = %d$ MHz', Variable.bandwidth(iBandwidth) / 1e6), 'position', [0, 0, 500, 400]);
 	plotHandle = gobjects(1, 6);
 	hold all;
 	plotHandle(1) = plot(reIdealIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reIdealIrs{iBandwidth}(2, :));
 	plotHandle(2) = plot(reAdaptiveIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reAdaptiveIrs{iBandwidth}(2, :));
-	plotHandle(3) = plot(reWitIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reWitIrs{iBandwidth}(2, :));
-	plotHandle(4) = plot(reWptIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reWptIrs{iBandwidth}(2, :));
-	plotHandle(5) = plot(reRandomIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reRandomIrs{iBandwidth}(2, :));
-	plotHandle(6) = plot(reNoIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reNoIrs{iBandwidth}(2, :));
+	plotHandle(3) = plot(reLinearIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reLinearIrs{iBandwidth}(2, :));
+	plotHandle(4) = plot(reWitIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reWitIrs{iBandwidth}(2, :));
+	plotHandle(5) = plot(reWptIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reWptIrs{iBandwidth}(2, :));
+	plotHandle(6) = plot(reRandomIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reRandomIrs{iBandwidth}(2, :));
+	plotHandle(7) = plot(reNoIrs{iBandwidth}(1, :) / nSubbands, 1e6 * reNoIrs{iBandwidth}(2, :));
 	hold off;
 	grid on;
-	legend('Ideal FS IRS', 'Adaptive IRS', 'WIT-optimized IRS', 'WPT-optimized IRS', 'Random IRS', 'No IRS');
+	legend('Ideal FS IRS', 'Adaptive nonlinear IRS', 'Adaptive linear IRS', 'WIT-optimized IRS', 'WPT-optimized IRS', 'Random IRS', 'No IRS');
 	xlabel('Per-subband rate [bps/Hz]');
 	ylabel('DC current [$\mu$A]');
 	xlim([0 inf]);
